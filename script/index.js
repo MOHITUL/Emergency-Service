e1fd9c199279e5ed752cbdc044952e57a454ac55
MOHITUL <mohitulislam9678@gmail.com>
2025-08-28 21:22:39 +0600

    added call section

--- script/index.js
+++ script/index.js
@@ -1,3 +1,4 @@
+// heart count
 const heartCountElement = document.getElementById("heartCount");
 let count = 0;
 
@@ -9,3 +10,34 @@ heartButtons.forEach(btn =>{
         heartCountElement.textContent = count;
     })
 })
+
+// call
+let coins = 100;
+const coinCountElement = document.getElementById("coinCount");
+const callHistoryElement = document.getElementById("callHistory");
+const clearHistoryElement = document.getElementById("clearHistory");
+
+const callButtons = document.querySelectorAll(".call-btn");
+
+callButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        const card = btn.closest(".bg-white");
+        const serviceName = card.querySelector(".service-name").textContent;
+        const serviceNumber = card.querySelector(".service-number").textContent;
+
+        if(coins < 20){
+            alert("Don't Have Enough Coins!");
+            return;
+        }
+
+        coins -= 20;
+        coinCountElement.textContent = coins;
+
+        alert(`Calling ${serviceName} (${serviceNumber})...`);
+
+        const li = document.createElement("li");
+        li.textContent = `${serviceName} - ${serviceNumber}`;
+        callHistory.appendChild(li);
+    })
+})
+
